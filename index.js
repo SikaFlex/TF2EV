3aca8b794e1ff3620cd29fee4581f54b2d03a377
angel.garcia <angel.garcia@axisdata.net>
2023-04-04 12:48:52 +0200

    Trabajo

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
 const express = require("express");
-
+const bodyParser= require("body-parser");
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extends:true}));
 
-const productos = [
+//Stock de productos
+let productos = [
 
     {
     id: 1,
-    nombre: "Helmo",
+    nombre: "Egipto",
     precio: 50,
-    imagen: "img/descarga.jpg",
-    stock: 50,
+    imagen: "img/desert.jpg",
+    stock: 5,
   },
     {
     id: 2,
-    nombre: "pepe",
+    nombre: "Islandia",
     precio: 50,
-    imagen: "img/descarga.jpg",
-    stock: 50,
+    imagen: "img/island.jpg",
+    stock: 6,
   },
     {
     id: 3,
-    nombre: "Raimon",
+    nombre: "Noruega",
     precio: 50,
-    imagen: "img/descarga.jpg",
-    stock: 50,
+    imagen: "img/nor.jpg",
+    stock: 10,
   },
     {
     id: 4,
-    nombre: "Tumadre",
+    nombre: "España",
     precio: 50,
-    imagen: "img/descarga.jpg",
-    stock: 50,
+    imagen: "img/spain.jpg",
+    stock: 8,
   },
     {
     id: 5,
-    nombre: "Helmo",
+    nombre: "Peru",
     precio: 50,
-    imagen: "img/descarga.jpg",
-    stock: 50,
+    imagen: "img/Machu.jpg",
+    stock: 9,
   },
     {
     id: 6,
-    nombre: "Helmo",
+    nombre: "Nueva York",
     precio: 50,
-    imagen: "img/descarga.jpg",
-    stock: 50,
+    imagen: "img/ny.jpg",
+    stock: 7,
   },
  
 ];
@@ -58,15 +60,24 @@ app.get("/api/productos", (req, res) => {
 
 app.use("/", express.static("front"));
 
+
+//funcion del carro para pagar
 app.post("/api/pay", (req, res) => {
     const ids = req.body;
+    const copiaProductos=productos.map(p=>({...p}));
     ids.forEach(id => {
-      const producto = productos.find(p => p.id === id);
-      if (producto) {
+      const producto = copiaProductos.find(p => p.id === id);
+       
+      if (producto.stock>0) {
         producto.stock--;
       }
+      else{
+        throw("No hay stock");
+      }
     });
-    res.send(productos);
+    productos=copiaProductos.find(p=>p.id==id);
+    res.send(producto);
+    
   });
   
 app.listen(port, () => {
