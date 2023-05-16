51ad33d4617642f8d30984f05e51f2b4359d0e22
sikaflex <pesadiya.gb@gmail.com>
2023-05-16 17:24:28 +0200

    portatil 16/05

--- mongodb.js
+++ mongodb.js
@@ -5,11 +5,7 @@ const {model,Schema}=mongoose
 
 mongoose.connect(url)
   .then(() => {
-<<<<<<< HEAD
-    console.log('La base de datos está conectada mongo');
-=======
     console.log('La base de datos está conectada');
->>>>>>> 4755ffff47a58ac471bc47f042812a99f7d892b2
   })
   .catch((err) => {
     console.error(err);
